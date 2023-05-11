b716b72f803c0fe818a436cb515d2a5a83f15608
FobosGuy <fobosguychanel@gmail.com>
2023-05-11 18:08:08 +0300

    Removed logs in the script

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,5 @@
-console.log('Hello!')
-
 const elBody = document.querySelector('body')
 const toggle = document.querySelector('#dark')
-
-console.log(elBody)
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches || localStorage.getItem('theme') == 'dark') {
   elBody.classList.add('dark')
@@ -13,7 +9,6 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
 toggle.addEventListener('pointerdown', e => {
   if (!toggle.checked) {
     elBody.classList.add('dark')
-    console.log('toggled')
     localStorage.setItem('theme', 'dark')
   } else {
     elBody.classList.remove('dark')
